69da9e2792242a7ad2047d28ce0ce380456ff83a
Hari <dnshariprasad@gmail.com>
2025-01-12 17:48:45 +0530

    fix

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -3,7 +3,7 @@ import { CenteredContainerVertical } from "../theme/KidStyles";
 const Welcome = () => {
   return (
     <CenteredContainerVertical>
-      <h1>Welcome Kiddoo</h1>
+      <h1>Welcome</h1>
     </CenteredContainerVertical>
   );
 };
