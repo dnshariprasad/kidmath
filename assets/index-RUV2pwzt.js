99f57954d553227cd006cd9db015d3ffcc2870c4
Hari <dnshariprasad@gmail.com>
2026-04-22 19:38:35 +0530

    Updates

---
+++ assets/index-RUV2pwzt.js
@@ -0,0 +1,1 @@
+import{an as n}from"./index-Cyde9GVd.js";function c(t,s="en-US"){if(n.getState().alphabet.isMuted)return;window.speechSynthesis.cancel();const e=new SpeechSynthesisUtterance(t);e.lang=s,e.rate=.95,e.pitch=1.1,window.speechSynthesis.speak(e)}export{c as r};
